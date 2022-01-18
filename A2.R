e41c262a473d92bf0e44347c69faefe239a45b6e
Joel Sagman <joelsmac@Joels-MBP.cogeco.local>
2022-01-18 15:29:45 -0500

    Finished part1. Added data set and citation for Part 2.

--- A2.R
+++ A2.R
@@ -3,23 +3,32 @@
 # 1. A regular save rewrite the current version of the file. 
 #To commit something with Git is similar to saving but it allows for version control tracking.
 #Each commit acts as a record the can be gone back too or used to restore work
-#Branching allows for two seperate commit historys to exsist in parallel.
+#Branching allows for two separate commit history to exist in parallel.
 #This branched versions can then be merge into the same commit file. 
 
 # 2. Commits through Git allow for version control which can be shared with other users.
 #It is also an easy way to track changes.
 
-# 3.
+# 3. .gitignore contains files that Git has been told by the user to ignore. 
+#This includes any the user does not committed into the GitHub repository.
+#This means it will not show up in the official version.
 
-# 4.
+# 4. You would 'push' our official version of containing your Git files to GitHub 
+#and 'pull' changes from GitHub to synchronize with your version.
 
 # 5. Git a version control program allowing for commits 
+#is a local repository where GitHub is an online repository where
+#files can be pushed and pulled from. This allows for multiple people to work on
+#the same git repository
 
-#is a local repository where GitHub is an only repository that allows for multiple people to work on the same file. 
 
 # 6. @JoelSagman
 
-#Part 2
+#Part 2 
+#Kayal, Mohsen; Adjeroud, Mehdi (2022), Data from: 
+# the war of corals: patterns, drivers, and implications 
+#of changing coral competitive performances across reef environments, 
+#Dryad, Dataset, https://doi.org/10.5061/dryad.98sf7m0kn
 
 # 1. 
 
