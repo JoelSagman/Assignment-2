3f21084f753672f5d2bf08f0d875ebb0a1407407
Joel Sagman <joelsmac@Joels-MacBook-Pro.local>
2022-01-18 15:02:12 -0500

    Commiting working files. I think this is how it is done?

---
+++ A2.R
@@ -0,0 +1,32 @@
+# Part 1
+
+# 1. A regular save rewrite the current version of the file. 
+#To commit something with Git is similar to saving but it allows for version control tracking.
+#Each commit acts as a record the can be gone back too or used to restore work
+#Branching allows for two seperate commit historys to exsist in parallel.
+#This branched versions can then be merge into the same commit file. 
+
+# 2. Commits through Git allow for version control which can be shared with other users.
+#It is also an easy way to track changes.
+
+# 3.
+
+# 4.
+
+# 5. Git a version control program allowing for commits 
+
+#is a local repository where GitHub is an only repository that allows for multiple people to work on the same file. 
+
+# 6. @JoelSagman
+
+#Part 2
+
+# 1. 
+
+# 2. 
+
+# 3. 
+
+# 4. 
+
+# 5.
